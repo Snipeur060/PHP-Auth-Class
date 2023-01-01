9965299d22da5acceda7e8820d61845b3d9d2da7
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2023-01-01 11:57:59 +0100

    Create auth-class.php

---
+++ auth-class.php
@@ -0,0 +1,50 @@
+<?php
+
+class Auth
+{
+    private $db; // database connection
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function register($username, $password)
+    {
+        // Hash the password using the password_hash function
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        // Insert the new user into the database
+        $query = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+    }
+
+    public function login($username, $password)
+    {
+        // Get the user from the database
+        $query = "SELECT * FROM users WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+
+        // If the user was found, check the password
+        if ($user) {
+            if (password_verify($password, $user['password'])) {
+                // Password is correct, log the user in
+                return true;
+            } else {
+                // Password is incorrect
+                return false;
+            }
+        } else {
+            // User was not found
+            return false;
+        }
+    }
+}
+
+?>
